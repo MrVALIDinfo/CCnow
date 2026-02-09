e2a74c3fd189850eed1fd85489809bf5cea950cf
MrVALIDinfo <mrvalidinfo@gmail.com>
2026-02-09 22:41:41 +0600

    adding test api

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,9 +8,16 @@ import { viteSingleFile } from "vite-plugin-singlefile";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), viteSingleFile()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8000",
+        changeOrigin: true,
+      },
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
